e7928565cdf45fd6b23e67ab8878358fd06231d4
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-11-27 18:20:09 +0100

    Add post

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
-# was downloaded and placed in this directory instead of sculpin having been
-# installed globally.
+mv docs/CNAME /tmp
 
-sculpin generate --env=prod
-if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
+vendor/bin/sculpin generate --env=prod --clean
 
-# Add --delete right before "output_prod" to have rsync remove files that are
-# deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
-if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+rm -rf docs
+mv output_prod docs
+mv /tmp/CNAME docs
+
+git add .
+git commit -m "Add post"
+git push origin master
